ebb4dde85f56fc2dd2ecbfb0aa23408cb9f6fd94
Serge Renfer <serge.renfer@gmail.com>
2011-09-19 13:17:54 +0200

    end sommer 2011 version

--- ageliaco/rd/Extensions/transitionscripts.py
+++ ageliaco/rd/Extensions/transitionscripts.py
@@ -5,14 +5,41 @@ def schoolDirSend(self, state_change):
     print "schoolDirSend called !!!"
     #pass
     
+def schoolDirApprove(self, state_change):
+    """ school's director sends an email to the contributor to inform him """
+    print "schoolDirApprove called !!!"
+    #pass
+    
 def DIPDirSend(self, state_change):
     """ sends an email to the DIP's responsible for RD (Andenmatten) """
     print "DIPDirSend called !!!"
     #pass
     
-def testAllAttributions(self, state_change):
+    
+def acceptAttribution(self, state_change):
     """ tests if all attribution objects in this projet are published
     If it's the case : it sends an email to contributors and supervisors """
+    print "acceptAttribution called !!!"
+    #pass
+    
+    
+def rejectAttribution(self, state_change):
+    """ schoolDir reject attribution 
+    it sends an email to the contributor and RD """
+    print "rejectAttribution called !!!"
+    #pass
+    
+def reviseAttribution(self, state_change):
+    """ schoolDir ask for a revision of the attribution 
+    it sends an email to the contributor and RD """
+    print "reviseAttribution called !!!"
+    #pass
+    
+def testAllAttributions(self, state_change):
+    """ tests if all attribution objects in this projet are accepted (peut être un mix 
+    entre accepted et renounced)
+    If it's the case : it sends an email to contributors, schools and supervisors 
+    et change l'état du projet à "on" (en cours)"""
     print "testAllAttributions called !!!"
     #pass
     
